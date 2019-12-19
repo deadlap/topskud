5460519d5fd6770f25a6ad36046fd3a14ec083b5
deadlap <llabildgaard@hotmail.com>
2019-12-19 11:47:13 +0100

    Somewhat fixed bullet penetration through walls

--- src/obj/bullet.rs
+++ src/obj/bullet.rs
@@ -63,7 +63,7 @@ impl<'a> Bullet<'a> {
                 }
                 bullet_obj.pos = cast.into_point();
             }
-
+            
             Ok(())
         } else {
             let img = a.get_img(ctx, self.weapon.bullet_type.get_spr());
@@ -78,57 +78,50 @@ impl<'a> Bullet<'a> {
         if let BulletType::SawBlade = self.weapon.bullet_type {
             self.obj.rot += 0.08;
         }
-        if self.vel.norm() < 375. {
+        let mut min_vel = 650.;
+        if let BulletType::SawBlade = self.weapon.bullet_type{
+            min_vel = 350.;
+        }
+        if self.vel.norm() < min_vel {
             return Hit::Wall;
         }
-        // Tjek om kuglen rammer spilleren
-        // Da distancen kuglen rejser er et linjestykke, og spilleren er en cirkel, bruger vi `dist_line_circle`
-        if Grid::dist_line_circle(start, d_pos, player.obj.pos) <= 16. {
-            let hs = self.apply_damage(&mut player.health, player.obj.pos);
-            return Hit::Player(hs);
-        }
-        // Tjek om kuglen rammer en enemy
-        // Samme fremgangsmåde som med spilleren
-        for (i, enem) in enemies.iter_mut().enumerate() {
-            if Grid::dist_line_circle(start, d_pos, enem.pl.obj.pos) <= 16. {
-                if self.in_enemy.is_none() {
-                    let hs = self.apply_damage(&mut enem.pl.health, enem.pl.obj.pos);
-                    self.in_enemy = Some(i);
-                    return Hit::Enemy(i, hs);
-                }
-            } else if let Some(j) = self.in_enemy {
-                if i == j {
-                    self.in_enemy = None;
-                }
-            }
-        }
         let cast = grid.ray_cast(palette, start, d_pos, true);
-        if !self.weapon.bullet_type.bouncy() {
-            let (bx,by) = Grid::snap(start+d_pos);
-            let r_mat = Grid::get(grid, bx, by);
-            let (nx,ny) = Grid::snap(start);
-            let n_mat = Grid::get(grid, nx, ny);
-            if !cast.full(){
-                if Grid::is_solid(grid, palette, bx, by) {
-                    self.in_wall = r_mat;
-                } else if Grid::is_solid(grid, palette, nx, ny) {
-                    self.in_wall = n_mat;
-                } else {
-                    self.in_wall = None;
-                }
+        if cast.full() || self.weapon.bullet_type.bouncy() {
+            self.in_wall = None;
+        } else {
+            let dir = angle_to_vec(self.obj.rot);
+            let (cur_x,cur_y) = Grid::snap(cast.into_point()+dir);
+            let cur_mat = Grid::get(grid, cur_x, cur_y);
+            if Grid::is_solid(grid, palette, cur_x, cur_y) {
+                self.in_wall = cur_mat;
+            } else {
+                self.in_wall = None;
             }
-            if self.in_wall.is_some() {
-                let rob_mat = palette.get_robust(self.in_wall.unwrap());
-                let mat_req = (self.vel.norm()*(1.0-rob_mat) / self.weapon.bullet_speed)*self.weapon.penetration;
-                if rob_mat <= mat_req {
-                    self.vel *= 1.0-(1.0-self.weapon.penetration)*rob_mat;//1.0-rob_mat;
-                    self.obj.pos += d_pos;
-                    return Hit::None
+        }
+        if self.in_wall.is_none() {
+            // Tjek om kuglen rammer spilleren
+            // Da distancen kuglen rejser er et linjestykke, og spilleren er en cirkel, bruger vi `dist_line_circle`
+            if Grid::dist_line_circle(start, d_pos, player.obj.pos) <= 16. {
+                let hs = self.apply_damage(&mut player.health, player.obj.pos);
+                return Hit::Player(hs);
+            }
+            // Tjek om kuglen rammer en enemy
+            // Samme fremgangsmåde som med spilleren
+            for (i, enem) in enemies.iter_mut().enumerate() {
+                if Grid::dist_line_circle(start, d_pos, enem.pl.obj.pos) <= 16. {
+                    if self.in_enemy.is_none() {
+                        let hs = self.apply_damage(&mut enem.pl.health, enem.pl.obj.pos);
+                        self.in_enemy = Some(i);
+                        return Hit::Enemy(i, hs);
+                    }
+                } else if let Some(j) = self.in_enemy {
+                    if i == j {
+                        self.in_enemy = None;
+                    }
                 }
             }
         }
         self.obj.pos = cast.into_point();
-
         if self.weapon.bullet_type.bouncy() {
             // to_wall er en vektor fra bulletens nuværende position til dér, hvor den vil ramme væggen.
             if let Some(to_wall) = cast.half_vec() {
@@ -136,12 +129,20 @@ impl<'a> Bullet<'a> {
 
                 let clip = cast.clip();
                 self.obj.pos += clip -  2. * clip.dot(&to_wall)/to_wall.norm_squared() * to_wall;
-                self.vel -= 2. * self.vel.dot(&to_wall)/to_wall.norm_squared() * to_wall;
+                self.vel -= 2. * self.vel.dot(&to_wall)/to_wall.norm_squared() * to_wall - 2. * d_vel;
                 self.obj.rot = angle_from_vec(self.vel);
             }
-
             // Hvis bulleten er bouncy, rammer den aldrig en væg.
             Hit::None
+        } else if !self.weapon.bullet_type.bouncy() && self.in_wall.is_some() {
+            let material_robustness = palette.get_robust(self.in_wall.unwrap());
+            self.vel*=1.-material_robustness;
+            if material_robustness <= self.weapon.penetration && self.vel.norm() >= min_vel {
+                self.obj.pos += cast.clip();
+                Hit::None
+            } else {
+                Hit::Wall
+            }
         } else if cast.full() {
             // Hvis castet er fuldt, har den ikke ramt noget
             Hit::None
